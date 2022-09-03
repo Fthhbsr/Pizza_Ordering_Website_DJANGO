fd29610e2aafc1fcb81db617feec571279e6f0ed
fatih <fthhbsr@gmail.com>
2022-09-03 17:24:24 +0300

    updated

---
+++ pizza/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from pizza.models import Pizza
+
+
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields= ['topping1', 'topping2', 'size']
+        labels = {'topping1':'Topping_1', 'topping2':'Topping_2', 'size': 'Size'}
+
+
+class MultiOrderingForm(forms.Form):
+    number=forms.IntegerField(required=False)
+
+# class MultiOrderingForm(forms.Form):
+#     class Meta:
+#         model:Quant
+#         fields='__all__'
--- pizza/views.py
+++ pizza/views.py
@@ -1,5 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from pizza.forms import MultiOrderingForm, PizzaForm
 
 # Create your views here.
 def home(request):
     return render(request, 'pizza/home.html')
+
+def ordering_pizza(request):
+    form = PizzaForm()
+    form_multi = MultiOrderingForm(request.POST or None)
+    number = dict(request.POST)
+    print(number)
+    if request.method == 'POST':
+        form = PizzaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={
+        'form': form,
+        'form_multi': form_multi
+    }
+    return render(request, 'pizza/order.html', context)
+
+def multi_order(request):
+    form = PizzaForm()
+    form_multi = MultiOrderingForm(request.POST or None)
+    if form_multi.is_valid():
+        form_multi.save()
+        return redirect('home')
+    context = {
+        'form' : form,
+        'form_multi': form_multi
+    }
+    return render(request, 'pizza/pizzas.html', context)
+
+# def order(request):
+#     form = PizzaForm()
+#     if request.method == 'POST':
+#         form = PizzaForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     context = {
+#         'form' : form,
+#     }
+#     return render(request, 'pizza/order.html', context)
+
+
+
+# def multi_order(request):
+#     form_multi = MultiOrderingForm()
+#     if request.method == 'POST':
+#         form_multi = MultiOrderingForm(request.POST)
+#         if form_multi.is_valid():
+#             form_multi.save()
+#             return redirect('pizzas')
+#     context = {
+#         'form_multi':form_multi,
+#     }
+#     return render(request, 'pizza/pizzas.html', context)
